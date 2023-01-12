3d1062e02babf0db05834800b8e4d55048b32f14
JamesCZ98 <james.coppock98@gmail.com>
2023-01-11 23:54:30 -0600

    Add responsive navbar
    Navbar now shrinks to a sidenav on small screens

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,18 +1,46 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger(
+      'show-side-nav', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('300ms', style({ transform: 'translateX(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0)', opacity: 1 }),
+        animate('300ms', style({ transform: 'translateX(100%)', opacity: 0 }))
+      ])
+    ]
+    )
+  ]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  isNavbarCollapsed: boolean = false;
+  isNavbarCollapsed: boolean = true;
 
   constructor(readonly toastr: ToastrService) { }
 
-  ngOnInit(): void {
-  }
+  menu: MenuLink[] = [
+    {
+      title: 'Home',
+      link: '/'
+    },
+    {
+      title: 'Get To Know Us',
+      link: '/get-to-know-us'
+    }
+  ]
 
+}
+
+interface MenuLink {
+  title: string
+  link: string
 }
